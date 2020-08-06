9d99e5283ec526ff009726f871be2e885b0b7948
Zeeshan <zeeshan@guiker.com>
2020-08-06 14:12:32 +0530

    add player screen with minimal logcs

--- src/components/PlayerContainer/PlayerContainer.tsx
+++ src/components/PlayerContainer/PlayerContainer.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import clsx from 'clsx';
+import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import { useMediaQuery } from 'react-responsive';
+import { H5, H6 } from 'ui-neumorphism';
 import { CustomTheme } from '../../theme';
+import { PlayerStatus, Status } from '../../utils/PlayerStatus';
 import { ButtonContainer } from '../ButtonContainer/ButtonContainer';
+import { CardContainer } from '../CardContainer/CardContainer';
 
 export interface PlayerContainerProps {}
 
@@ -11,16 +16,72 @@ const useStyles = createUseStyles((theme: CustomTheme) => ({
     height: '100%',
     width: '100%',
     gridTemplateColumns: '1fr',
-    gridTemplateRows: '3fr 1fr',
+    gridTemplateRows: '0.2fr 3fr 1fr',
+  },
+  playersContainer: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: '1fr',
+    padding: theme.spacing(2),
+  },
+  playersContainerMobile: {
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '1fr 1fr',
+  },
+  cardContainer: {
+    margin: theme.spacing(1),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    marginTop: theme.spacing(1),
+    fontWeight: 'bold',
+  },
+  blueText: {
+    color: theme.colors.blue,
+  },
+  greenText: {
+    color: theme.colors.green,
   },
 }));
 
 const PlayerContainer: React.FC<PlayerContainerProps> = () => {
   const classes = useStyles();
+  const [mode, setMode] = useState<'Normal' | 'Tactical'>('Normal');
+  const isMobile: boolean = useMediaQuery({ maxWidth: 780 });
+  const [player1Status, setPlayer1Status] = useState<Status>('Rock');
+  const [player2Status, setPlayer2Status] = useState<Status>('Rock');
+  const handleMode = () => (mode === 'Normal' ? setMode('Tactical') : setMode('Normal'));
+  const handleRock = () => setPlayer1Status('Rock');
+  const handlePaper = () => setPlayer1Status('Paper');
+  const handleScissors = () => setPlayer1Status('Scissors');
+
   return (
     <div className={classes.mainContainer}>
-      <div>this is player container</div>
-      <ButtonContainer />
+      <H5 className={classes.header}>
+        Mode : <span className={mode === 'Normal' ? classes.greenText : classes.blueText}>{mode}</span>
+      </H5>
+      <div className={clsx([classes.playersContainer, isMobile && classes.playersContainerMobile])}>
+        <CardContainer inset={true} cardStyle={classes.cardContainer}>
+          {PlayerStatus(player1Status as Status)}
+          <H6>Player 1 : Rock</H6>
+        </CardContainer>
+        <CardContainer inset={true} cardStyle={classes.cardContainer}>
+          {PlayerStatus(player2Status)}
+          <H6>Computer : Rock</H6>
+        </CardContainer>
+      </div>
+      <ButtonContainer
+        handleRock={handleRock}
+        handlePaper={handlePaper}
+        handleScissors={handleScissors}
+        handleTactical={handleMode}
+        tacticalButtonText={mode}
+      />
     </div>
   );
 };
